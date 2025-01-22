b99a3e6fee82819bfdf9862dd7a058541ce7cd20
Sayemsaadat0 <sayemsaadat0@gmail.com>
2025-01-22 14:33:29 +0600

    transition

---
+++ src/components/HomeContainer.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const HomeContainer = () => {
+  return (
+    <div>
+      <div className='square'></div>
+    </div>
+  )
+}
+
+export default HomeContainer
